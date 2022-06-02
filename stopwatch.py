8ded8954a46ba3f615d4fe95c232cd962cba915a
tidues <tidues@gmail.com>
2022-06-02 11:56:59 -0400

    small changes

--- stopwatch.py
+++ stopwatch.py
@@ -1,10 +1,19 @@
 import time
+from datetime import datetime
+
+
+# get datetime string
+def dtstr():
+    now = datetime.now()
+    return now.strftime("%m%d%Y%H%M%S"), now
+
 
 class Stopwatch:
-    def __init__(self, name='stopwatch', on=True):
+    def __init__(self, name='stopwatch', digits=None, on=True):
         self.name = name
         self.on = on
         self.hist = {}
+        self.digits = digits
         self.lap_idx = 0
 
     def start(self):
@@ -13,6 +22,7 @@ class Stopwatch:
         self.lap_idx = 0
         self.hist = {}
         self.hist['start'] = time.time()
+        return self.hist['start']
 
     def lap(self, name=None):
         if not self.on:
@@ -21,19 +31,31 @@ class Stopwatch:
         if name is None:
             name = 'lap' + str(self.lap_idx)
         self.hist[name] = time.time()
+        return self.hist[name]
 
     # histType: 0: absolute times; 1: relative times; 2: time lapse
-    def getHist(self, histType=1):
+    def info(self, histType=1):
         if not self.on:
             return 0
         if histType == 0:
-            return self.hist
+            res = self.hist
         elif histType == 1:
             keys = list(self.hist)
-            return {keys[i]: self.hist[keys[i]] - self.hist[keys[0]] for i in range(1, len(keys))}
+            res = {keys[i]: self.hist[keys[i]] - self.hist[keys[0]] for i in range(1, len(keys))}
         else:
             keys = list(self.hist)
-            return {keys[i]: self.hist[keys[i]] - self.hist[keys[i-1]] for i in range(1, len(keys))}
+            res = {keys[i]: self.hist[keys[i]] - self.hist[keys[i-1]] for i in range(1, len(keys))}
+        if self.digits is not None:
+            res = {key: round(res[key], self.digits) for key in res}
+        return res
+
+    # get total time
+    def total_time(self):
+        keys = list(self.hist)
+        res = self.hist[keys[-1]] - self.hist[keys[0]]
+        if self.digits is not None:
+            res = round(res, self.digits)
+        return res
 
 
 def main():
